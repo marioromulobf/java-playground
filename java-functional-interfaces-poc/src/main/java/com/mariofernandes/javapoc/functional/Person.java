7a5ca7c22872790a0d4a5b01b07b03768c0d2247
Mario Fernandes <marioromulof@gmail.com>
2025-10-28 22:06:00 -0300

    feat(java): add person class

---
+++ java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/Person.java
@@ -0,0 +1,34 @@
+package com.mariofernandes.javapoc.functional;
+
+public class Person {
+    private String name;
+    private int age;
+    private String country;
+
+    public Person(String name, int age, String country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + "', " +
+                "age=" + age + ", " +
+                "country='" + country + '\'' +
+                '}';
+    }
+}
