163a0ace98a0e691e61a1aa29611fea9929107be
Mario Fernandes <marioromulof@gmail.com>
2025-11-04 10:45:09 -0300

    feat(java): first commit of Consumer

---
+++ java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/ConsumerExamples.java
@@ -0,0 +1,19 @@
+package com.mariofernandes.javapoc.functional;
+
+public class ConsumerExamples {
+
+    public static void basic() {
+        System.out.println(" --- Basic Consumer --- ");
+        System.out.println(" --- --- - --- --- ");
+    }
+
+    public static void biConsumer() {
+        System.out.println(" --- BiConsumer Example --- ");
+        System.out.println(" --- --- - --- --- ");
+    }
+
+    public static void whyDoNotWe() {
+        System.out.println(" --- Suggestions --- ");
+        System.out.println(" --- --- - --- --- ");
+    }
+}
