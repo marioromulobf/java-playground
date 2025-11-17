c45187c622dd1cdd183affb060e4ebbe65151acf
Mario Fernandes <marioromulof@gmail.com>
2025-11-16 21:33:43 -0300

    feat(java): first Supplier example

--- java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/SupplierExamples.java
+++ java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/SupplierExamples.java
@@ -1,8 +1,13 @@
 package com.mariofernandes.javapoc.functional;
 
+import java.util.function.Supplier;
+
 public class SupplierExamples {
     public static void basic() {
         System.out.println(" --- Basic Supplier --- ");
+        Supplier<String> stringSupplier = () -> "Hello, My Constant Supplier!";
+
+        System.out.println("stringSupplier.get() = " + stringSupplier.get());
     }
 
     public static void whyDoNotWe() {
