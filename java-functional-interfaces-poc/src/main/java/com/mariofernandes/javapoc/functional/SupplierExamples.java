03f4d6fc798ccc15d639947438dc7cbd755692fc
Mario Fernandes <marioromulof@gmail.com>
2025-11-17 22:00:40 -0300

    feat(java): DoubleSupplier example

--- java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/SupplierExamples.java
+++ java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/SupplierExamples.java
@@ -1,13 +1,18 @@
 package com.mariofernandes.javapoc.functional;
 
+import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
 public class SupplierExamples {
     public static void basic() {
         System.out.println(" --- Basic Supplier --- ");
         Supplier<String> stringSupplier = () -> "Hello, My Constant Supplier!";
+        DoubleSupplier randomDoubleSupplier = Math::random;
 
         System.out.println("stringSupplier.get() = " + stringSupplier.get());
+
+        System.out.println(" ---> DoubleSupplier ");
+        System.out.println("randomDoubleSupplier.getAsDouble() = " + randomDoubleSupplier.getAsDouble());
     }
 
     public static void whyDoNotWe() {
