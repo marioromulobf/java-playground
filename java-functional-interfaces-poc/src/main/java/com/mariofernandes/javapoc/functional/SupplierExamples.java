582993939ff2e8939e1bb368f6a5a089e1d88f55
Mario Fernandes <marioromulof@gmail.com>
2025-11-18 21:43:45 -0300

    feat(java): IntSupplier

--- java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/SupplierExamples.java
+++ java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/SupplierExamples.java
@@ -1,15 +1,21 @@
 package com.mariofernandes.javapoc.functional;
 
+import java.time.LocalDate;
 import java.util.function.DoubleSupplier;
+import java.util.function.IntSupplier;
 import java.util.function.Supplier;
 
 public class SupplierExamples {
     public static void basic() {
         System.out.println(" --- Basic Supplier --- ");
         Supplier<String> stringSupplier = () -> "Hello, My Constant Supplier!";
+        IntSupplier dayOfMonthIntSupplier = () -> LocalDate.now().getDayOfMonth();
         DoubleSupplier randomDoubleSupplier = Math::random;
 
         System.out.println("stringSupplier.get() = " + stringSupplier.get());
+
+        System.out.println(" ---> IntSupplier ");
+        System.out.println("dayOfMonthIntSupplier.getAsInt() = " + dayOfMonthIntSupplier.getAsInt());
 
         System.out.println(" ---> DoubleSupplier ");
         System.out.println("randomDoubleSupplier.getAsDouble() = " + randomDoubleSupplier.getAsDouble());
