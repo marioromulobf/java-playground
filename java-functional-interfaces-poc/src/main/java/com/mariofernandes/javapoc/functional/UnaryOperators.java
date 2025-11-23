682acc3d12544eb3f8a0f86b9a225154180a90c4
Mario Fernandes <marioromulof@gmail.com>
2025-11-22 23:36:07 -0300

    feat(java): basic UnaryOpertator

--- java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/UnaryOperators.java
+++ java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/UnaryOperators.java
@@ -1,9 +1,18 @@
 package com.mariofernandes.javapoc.functional;
 
+import java.util.function.UnaryOperator;
+
 public class UnaryOperators {
 
     public static void basic() {
         System.out.println(" --- Basic UnaryOperator --- ");
+        UnaryOperator<String> toUpperCase = String::toUpperCase;
+        UnaryOperator<String> reverseString = s -> new StringBuilder(s).reverse().toString();
+
+
+        System.out.println("toUpperCase.apply(\"Mario\") = " + toUpperCase.apply("Mario"));
+        System.out.println("reverseString.andThen(toUpperCase).apply(\"Mario\") = " + reverseString.andThen(toUpperCase).apply("Mario"));
+
         System.out.println(" --- --- - --- --- ");
     }
 
