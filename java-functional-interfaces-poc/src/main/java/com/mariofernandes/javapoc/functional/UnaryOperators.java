6dd1d9ea35215e373b2d343caf4c2800c2568d32
Mario Fernandes <marioromulof@gmail.com>
2025-11-25 23:30:32 -0300

    feat(java): basic IntUnaryOpertator

--- java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/UnaryOperators.java
+++ java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/UnaryOperators.java
@@ -1,5 +1,6 @@
 package com.mariofernandes.javapoc.functional;
 
+import java.util.function.IntUnaryOperator;
 import java.util.function.UnaryOperator;
 
 public class UnaryOperators {
@@ -10,6 +11,10 @@ public class UnaryOperators {
         UnaryOperator<String> reverseString = s -> new StringBuilder(s).reverse().toString();
         UnaryOperator<String> toLowerCase = String::toLowerCase;
         UnaryOperator<String> identity = UnaryOperator.identity();
+        IntUnaryOperator incrementOne = i -> i + 1;
+        IntUnaryOperator subtractOne = i -> i - 1;
+        IntUnaryOperator square = i -> i * i;
+        IntUnaryOperator identityInt = IntUnaryOperator.identity();
 
 
         System.out.println("toUpperCase.apply(\"Mario\") = " + toUpperCase.apply("Mario"));
@@ -17,6 +22,13 @@ public class UnaryOperators {
         System.out.println("reverseString.andThen(toUpperCase).apply(\"Mario\") = " + reverseString.andThen(toUpperCase).apply("Mario"));
         System.out.println("toLowerCase.compose(reverseString.compose(toUpperCase)).apply(\"Mario\") = " + toLowerCase.compose(reverseString.compose(toUpperCase)).apply("Mario"));
         System.out.println("identity.apply(\"Mario\") = " + identity.apply("Mario"));
+
+        System.out.println(" ---> IntUnaryOperator ");
+        System.out.println("incrementOne.applyAsInt(10) = " + incrementOne.applyAsInt(10));
+        System.out.println(" ---> IntUnaryOperator andThen|compose|identity ");
+        System.out.println("incrementOne.andThen(square.andThen(subtractOne)).applyAsInt(1) = " + incrementOne.andThen(square.andThen(subtractOne)).applyAsInt(1));
+        System.out.println("incrementOne.compose(square.compose(subtractOne)).applyAsInt(1) = " + incrementOne.compose(square.compose(subtractOne)).applyAsInt(1));
+        System.out.println("identityInt.applyAsInt(13) = " + identityInt.applyAsInt(13));
 
         System.out.println(" --- --- - --- --- ");
     }
