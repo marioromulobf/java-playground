b8f0a3632b85a5d0aa6b512e57b9363b087b57b7
Mario Fernandes <marioromulof@gmail.com>
2025-11-26 22:52:25 -0300

    feat(java): basic LongUnaryOpertator

--- java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/UnaryOperators.java
+++ java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/UnaryOperators.java
@@ -1,6 +1,7 @@
 package com.mariofernandes.javapoc.functional;
 
 import java.util.function.IntUnaryOperator;
+import java.util.function.LongUnaryOperator;
 import java.util.function.UnaryOperator;
 
 public class UnaryOperators {
@@ -15,6 +16,10 @@ public class UnaryOperators {
         IntUnaryOperator subtractOne = i -> i - 1;
         IntUnaryOperator square = i -> i * i;
         IntUnaryOperator identityInt = IntUnaryOperator.identity();
+        LongUnaryOperator multiplyByTwo = l -> l * 2L;
+        LongUnaryOperator subtractTwo = l -> l - 2L;
+        LongUnaryOperator divideByTwo = l -> l / 2L;
+        LongUnaryOperator identityLong = LongUnaryOperator.identity();
 
 
         System.out.println("toUpperCase.apply(\"Mario\") = " + toUpperCase.apply("Mario"));
@@ -30,6 +35,12 @@ public class UnaryOperators {
         System.out.println("incrementOne.compose(square.compose(subtractOne)).applyAsInt(1) = " + incrementOne.compose(square.compose(subtractOne)).applyAsInt(1));
         System.out.println("identityInt.applyAsInt(13) = " + identityInt.applyAsInt(13));
 
+        System.out.println(" ---> LongUnaryOperator ");
+        System.out.println("multiplayByTwo.applyAsLong(5L) = " + multiplyByTwo.applyAsLong(5L));
+        System.out.println(" ---> LongUnaryOperator andThen|compose|identity ");
+        System.out.println("multiplyByTwo.andThen(subtractTwo.andThen(divideByTwo)).applyAsLong(2L) = " + multiplyByTwo.andThen(subtractTwo.andThen(divideByTwo)).applyAsLong(2L));
+        System.out.println("multiplyByTwo.compose(subtractTwo.compose(divideByTwo)).applyAsLong(2L) = " + multiplyByTwo.compose(subtractTwo.compose(divideByTwo)).applyAsLong(2L));
+        System.out.println("identityLong.applyAsLong(20L) = " + identityLong.applyAsLong(20L));
         System.out.println(" --- --- - --- --- ");
     }
 
