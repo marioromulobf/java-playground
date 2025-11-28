33869ed9fd4c3af19da8b075091dbd7491d076fc
Mario Fernandes <marioromulof@gmail.com>
2025-11-28 10:21:09 -0300

    feat(java): basic DoubleUnaryOperator

--- java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/UnaryOperators.java
+++ java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/UnaryOperators.java
@@ -1,5 +1,6 @@
 package com.mariofernandes.javapoc.functional;
 
+import java.util.function.DoubleUnaryOperator;
 import java.util.function.IntUnaryOperator;
 import java.util.function.LongUnaryOperator;
 import java.util.function.UnaryOperator;
@@ -12,15 +13,21 @@ public class UnaryOperators {
         UnaryOperator<String> reverseString = s -> new StringBuilder(s).reverse().toString();
         UnaryOperator<String> toLowerCase = String::toLowerCase;
         UnaryOperator<String> identity = UnaryOperator.identity();
+
         IntUnaryOperator incrementOne = i -> i + 1;
         IntUnaryOperator subtractOne = i -> i - 1;
         IntUnaryOperator square = i -> i * i;
         IntUnaryOperator identityInt = IntUnaryOperator.identity();
+
         LongUnaryOperator multiplyByTwo = l -> l * 2L;
         LongUnaryOperator subtractTwo = l -> l - 2L;
         LongUnaryOperator divideByTwo = l -> l / 2L;
         LongUnaryOperator identityLong = LongUnaryOperator.identity();
 
+        DoubleUnaryOperator multiplyByHalf = d -> d * 0.5;
+        DoubleUnaryOperator sqrt = Math::sqrt;
+        DoubleUnaryOperator negate = d -> -d;
+        DoubleUnaryOperator identityDouble = DoubleUnaryOperator.identity();
 
         System.out.println("toUpperCase.apply(\"Mario\") = " + toUpperCase.apply("Mario"));
         System.out.println(" ---> UnaryOperator andThen|compose|identity ");
@@ -41,6 +48,13 @@ public class UnaryOperators {
         System.out.println("multiplyByTwo.andThen(subtractTwo.andThen(divideByTwo)).applyAsLong(2L) = " + multiplyByTwo.andThen(subtractTwo.andThen(divideByTwo)).applyAsLong(2L));
         System.out.println("multiplyByTwo.compose(subtractTwo.compose(divideByTwo)).applyAsLong(2L) = " + multiplyByTwo.compose(subtractTwo.compose(divideByTwo)).applyAsLong(2L));
         System.out.println("identityLong.applyAsLong(20L) = " + identityLong.applyAsLong(20L));
+
+        System.out.println(" ---> DoubleUnaryOperator ");
+        System.out.println("multiplyByHalf.applyAsDouble(5.0) = " + multiplyByHalf.applyAsDouble(5.0));
+        System.out.println(" ---> DoubleUnaryOperator andThen|compose|identity ");
+        System.out.println("multiplyByHalf.andThen(sqrt.andThen(negate)).applyAsDouble(25.0) = " + multiplyByHalf.andThen(sqrt.andThen(negate)).applyAsDouble(25.0));
+        System.out.println("multiplyByHalf.compose(sqrt.compose(negate)).applyAsDouble(-25.0) = " + multiplyByHalf.compose(sqrt.compose(negate)).applyAsDouble(-25.0));
+        System.out.println("identityDouble.applyAsDouble(13.5) = " + identityDouble.applyAsDouble(13.5));
         System.out.println(" --- --- - --- --- ");
     }
 
