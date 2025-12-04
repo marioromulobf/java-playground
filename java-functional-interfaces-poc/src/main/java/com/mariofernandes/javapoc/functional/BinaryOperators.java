1e6b031d40cead20770fafa3cc2fe107fa06f578
Mario Fernandes <marioromulof@gmail.com>
2025-12-03 23:50:32 -0300

    feat(java): apply BinaryOperator

--- java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/BinaryOperators.java
+++ java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/BinaryOperators.java
@@ -1,8 +1,15 @@
 package com.mariofernandes.javapoc.functional;
 
+import java.util.function.BinaryOperator;
+
 public class BinaryOperators {
     public static void basic() {
         System.out.println(" --- Basic BinaryOperator --- ");
+        BinaryOperator<Integer> sum = Integer::sum;
+
+        System.out.println("sum.apply(13, 51) = " + sum.apply(13, 51));
+
+
         System.out.println(" --- --- - --- --- ");
     }
 
