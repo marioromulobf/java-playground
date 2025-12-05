38fc46817ad708f128b35905786520e0b4612c75
Mario Fernandes <marioromulof@gmail.com>
2025-12-04 22:29:21 -0300

    feat(java): minBy and maxBy BinaryOperator

--- java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/BinaryOperators.java
+++ java-functional-interfaces-poc/src/main/java/com/mariofernandes/javapoc/functional/BinaryOperators.java
@@ -1,13 +1,19 @@
 package com.mariofernandes.javapoc.functional;
 
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 public class BinaryOperators {
     public static void basic() {
         System.out.println(" --- Basic BinaryOperator --- ");
         BinaryOperator<Integer> sum = Integer::sum;
+        BinaryOperator<Integer> integerMinBy = BinaryOperator.minBy(Integer::compare);
+        BinaryOperator<Integer> integerMaxBy = BinaryOperator.maxBy(Integer::compare);
 
         System.out.println("sum.apply(13, 51) = " + sum.apply(13, 51));
+        System.out.println(" ---> BinaryOperator minBy|maxBy ");
+        System.out.println("integerMinBy.apply(13, 51) = " + integerMinBy.apply(13, 51));
+        System.out.println("integerMaxBy.apply(13, 51) = " + integerMaxBy.apply(13, 51));
 
 
         System.out.println(" --- --- - --- --- ");
