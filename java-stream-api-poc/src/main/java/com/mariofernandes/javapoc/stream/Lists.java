4b1292a8e3ad39001403c11b7b4e7fd656d82a70
Mario Fernandes <marioromulof@gmail.com>
2025-12-18 22:35:12 -0300

    java(feat): commit Stream List

--- java-stream-api-poc/src/main/java/com/mariofernandes/javapoc/stream/Lists.java
+++ java-stream-api-poc/src/main/java/com/mariofernandes/javapoc/stream/Lists.java
@@ -5,17 +5,29 @@ import java.util.stream.Stream;
 
 public class Lists {
     public static void streams() {
-        List<String> names = List.of("Mario", "Ana", "Pedro", "Carla", "Maria");
-        names.stream()
-             .filter(name -> name.startsWith("M"))
-             .map(String::toUpperCase)
-             .forEach(System.out::println);
+        List<String> names = List.of("Mario", "Ana", "Pedro", "Mario", "Carla", "Maria");
+        System.out.println("List of names: ");
+        names.forEach(name -> System.out.println(" - " + name));
 
+        System.out.println("Sorted, limit by 3 and forEach of names (Collection stream): ");
         names.stream()
-             .sorted()
-             .forEach(System.out::println);
+                .sorted()
+                .limit(3)
+                .forEach(name -> System.out.println(" - " + name));
 
-        Stream.of(names).forEach(System.out::println);
-        Stream.of(names).sorted().forEach(System.out::println);
+        System.out.println("Distinct and forEach of names (Collection stream): ");
+        names.stream()
+                .distinct()
+                .forEach(name -> System.out.println(" - " + name));
+
+
+        System.out.println("Using Stream.of(List) and foreach: ");
+        Stream.of(names).forEach(name -> System.out.println(" - " + name));
+
+        System.out.println("Using Stream.of(List.toArray) and foreach: ");
+        Stream.of(names.toArray()).forEach(name -> System.out.println(" - " + name));
+
+        System.out.println("Using Stream.of(List.toArray), sorted and foreach: ");
+        Stream.of(names.toArray()).sorted().forEach(name -> System.out.println(" - " + name));
     }
 }
