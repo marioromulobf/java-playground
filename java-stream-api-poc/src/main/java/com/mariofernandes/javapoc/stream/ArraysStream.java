d35146afec77884d0cc655abdff566e08925ace7
Mario Fernandes <marioromulof@gmail.com>
2026-01-13 21:52:35 -0300

    feat(stream): stream arrays

---
+++ java-stream-api-poc/src/main/java/com/mariofernandes/javapoc/stream/ArraysStream.java
@@ -0,0 +1,11 @@
+package com.mariofernandes.javapoc.stream;
+
+import java.util.Arrays;
+
+public class ArraysStream {
+    public static final String[] NAMES = {"Mario", "Ana", "Pedro", "Mario", "Carla", "Maria"};
+
+    public ArraysStream() {}
+
+
+}
