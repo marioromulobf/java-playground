144ee1985f03b13c6ae6f9aa48cf161b981a4a3b
Mario Fernandes <marioromulof@gmail.com>
2026-01-27 00:05:35 -0300

    feat(stream): consumer :: map-mult

--- java-stream-api-poc/src/main/java/com/mariofernandes/javapoc/stream/ConsumersStream.java
+++ java-stream-api-poc/src/main/java/com/mariofernandes/javapoc/stream/ConsumersStream.java
@@ -1,12 +1,36 @@
 package com.mariofernandes.javapoc.stream;
 
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
 public class ConsumersStream {
 
+    public static final List<Person> PEOPLE = List.of(
+            new Person("Mario", 40, "Lisbon"),
+            new Person("Ana", 20, "Porto"),
+            new Person("Pedro", 9, "Coimbra"),
+            new Person("Mario", 40, "Porto"),
+            new Person("Carla", 101),
+            new Person("Maria", 35, "Porto")
+    );
+    private final BiConsumer<Person, Consumer<String>> biConsumer = (person, stringConsumer) -> stringConsumer.accept(person.name());
+
     public ConsumersStream() {}
+
+
+    public String operationsMapMult() {
+        return PEOPLE.stream().mapMulti(biConsumer).collect(Collectors.joining(", "));
+    }
 
     public static void run() {
         ConsumersStream consumersStream = new ConsumersStream();
 
+        System.out.println("People List: ");
+        PEOPLE.forEach(person -> System.out.println(" - " + person));
 
+        System.out.println("Operations: BiConsumer in mapMult to list name as String -> Results: ");
+        System.out.println(consumersStream.operationsMapMult());
     }
 }
--- java-stream-api-poc/src/test/java/com/mariofernandes/javapoc/stream/ConsumersStreamTest.java
+++ java-stream-api-poc/src/test/java/com/mariofernandes/javapoc/stream/ConsumersStreamTest.java
@@ -1,6 +1,9 @@
 package com.mariofernandes.javapoc.stream;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class ConsumersStreamTest {
     private ConsumersStream consumersStream;
@@ -8,5 +11,16 @@ public class ConsumersStreamTest {
     @BeforeEach
     void setup() {
         this.consumersStream = new ConsumersStream();
+    }
+
+    @Test
+    @DisplayName("Test Operations: MapMult")
+    void testOperationsMapMult_ReturnsExpectedValue() {
+        var result = consumersStream.operationsMapMult();
+        var expectedResult = "Mario, Ana, Pedro, Mario, Carla, Maria";
+
+        // Assertions
+        Assertions.assertInstanceOf(String.class, result, "Should return a String Instance");
+        Assertions.assertEquals(expectedResult, result, "Should return the correct names");
     }
 }
