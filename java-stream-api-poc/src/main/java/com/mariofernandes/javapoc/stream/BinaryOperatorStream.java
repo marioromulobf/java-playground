ffeeab97b947f33abd5a845ed2d8095c45b6a853
Mario Fernandes <marioromulof@gmail.com>
2026-01-29 23:09:49 -0300

    feat(stream): BinaryOperatorStream :: reduce

--- java-stream-api-poc/src/main/java/com/mariofernandes/javapoc/stream/BinaryOperatorStream.java
+++ java-stream-api-poc/src/main/java/com/mariofernandes/javapoc/stream/BinaryOperatorStream.java
@@ -1,11 +1,29 @@
 package com.mariofernandes.javapoc.stream;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+
 public class BinaryOperatorStream {
+
+    public static final List<String> NAMES = List.of("Mario", "Ana", "Pedro", "Mario", "Carla", "Maria");
+    private final BinaryOperator<String> binaryOperatorStringMinByLength = BinaryOperator.minBy(Comparator.comparingInt(String::length));
+
     public BinaryOperatorStream() {}
+
+    public Optional<String> operationsReduce() {
+        return NAMES.stream()
+                .reduce(binaryOperatorStringMinByLength);
+    }
 
     public static void run() {
         BinaryOperatorStream binaryOperatorStream = new BinaryOperatorStream();
+        System.out.println("Name List: ");
+        NAMES.forEach(name -> System.out.println(" - " + name));
 
+        System.out.println("Operations: BinaryOperator in reduce by String::length -> Result: ");
+        System.out.println(binaryOperatorStream.operationsReduce());
     }
 
 }
--- java-stream-api-poc/src/test/java/com/mariofernandes/javapoc/stream/BinaryOperatorStreamTest.java
+++ java-stream-api-poc/src/test/java/com/mariofernandes/javapoc/stream/BinaryOperatorStreamTest.java
@@ -1,6 +1,9 @@
 package com.mariofernandes.javapoc.stream;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class BinaryOperatorStreamTest {
     private BinaryOperatorStream binaryOperatorStream;
@@ -8,5 +11,19 @@ public class BinaryOperatorStreamTest {
     @BeforeEach
     void setup() {
         this.binaryOperatorStream = new BinaryOperatorStream();
+    }
+
+    @Test
+    @DisplayName("Test Operations: reduce")
+    void testOperationsReduce_ReturnsExpectedValue() {
+        var result = binaryOperatorStream.operationsReduce();
+        var expectedResult = "Ana";
+
+        // Assertions Optional
+        Assertions.assertTrue(result.isPresent(), "Should return a value");
+
+        // Assertions min by length
+        Assertions.assertEquals(expectedResult, result.get(),
+                "Should return the name with the shortest length: Ana");
     }
 }
