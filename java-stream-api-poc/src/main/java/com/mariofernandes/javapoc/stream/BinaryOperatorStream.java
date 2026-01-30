0d8694e016d7643c478345d14a154f7711acad92
Mario Fernandes <marioromulof@gmail.com>
2026-01-30 07:51:38 -0300

    feat(stream): BinaryOperatorStream :: reduce integer::sum

--- java-stream-api-poc/src/main/java/com/mariofernandes/javapoc/stream/BinaryOperatorStream.java
+++ java-stream-api-poc/src/main/java/com/mariofernandes/javapoc/stream/BinaryOperatorStream.java
@@ -7,23 +7,42 @@ import java.util.function.BinaryOperator;
 
 public class BinaryOperatorStream {
 
-    public static final List<String> NAMES = List.of("Mario", "Ana", "Pedro", "Mario", "Carla", "Maria");
+    public static final List<Person> PEOPLE = List.of(
+            new Person("Mario", 40, "Lisbon"),
+            new Person("Ana", 20, "Porto"),
+            new Person("Pedro", 9, "Coimbra"),
+            new Person("Mario", 40, "Porto"),
+            new Person("Carla", 101, "Braga"),
+            new Person("Maria", 35, "Porto")
+    );
+
     private final BinaryOperator<String> binaryOperatorStringMinByLength = BinaryOperator.minBy(Comparator.comparingInt(String::length));
+    private final BinaryOperator<Integer> binaryOperatorIntegerSum = Integer::sum;
 
     public BinaryOperatorStream() {}
 
-    public Optional<String> operationsReduce() {
-        return NAMES.stream()
+    public Optional<String> operationsMapReduceWithBinaryOperatorString() {
+        return PEOPLE.stream()
+                .map(Person::name)
                 .reduce(binaryOperatorStringMinByLength);
+    }
+
+    public Integer operationsMapReduceWithBinaryOperatorInteger() {
+        return PEOPLE.stream()
+                .map(Person::age)
+                .reduce(0, binaryOperatorIntegerSum);
     }
 
     public static void run() {
         BinaryOperatorStream binaryOperatorStream = new BinaryOperatorStream();
-        System.out.println("Name List: ");
-        NAMES.forEach(name -> System.out.println(" - " + name));
+        System.out.println("People List: ");
+        PEOPLE.forEach(person -> System.out.println(" - " + person));
 
         System.out.println("Operations: BinaryOperator in reduce by String::length -> Result: ");
-        System.out.println(binaryOperatorStream.operationsReduce());
+        System.out.println(binaryOperatorStream.operationsMapReduceWithBinaryOperatorString());
+
+        System.out.println("Operations: BinaryOperator in reduce by Integer::sum -> Result: ");
+        System.out.println(binaryOperatorStream.operationsMapReduceWithBinaryOperatorInteger());
     }
 
 }
--- java-stream-api-poc/src/test/java/com/mariofernandes/javapoc/stream/BinaryOperatorStreamTest.java
+++ java-stream-api-poc/src/test/java/com/mariofernandes/javapoc/stream/BinaryOperatorStreamTest.java
@@ -14,16 +14,25 @@ public class BinaryOperatorStreamTest {
     }
 
     @Test
-    @DisplayName("Test Operations: reduce")
-    void testOperationsReduce_ReturnsExpectedValue() {
-        var result = binaryOperatorStream.operationsReduce();
+    @DisplayName("Test Operations: BinaryOperator in reduce by String::length")
+    void testOperationsMapReduceWithBinaryOperatorString_ReturnsExpectedValue() {
+        var result = binaryOperatorStream.operationsMapReduceWithBinaryOperatorString();
         var expectedResult = "Ana";
 
-        // Assertions Optional
+        // Assertions
         Assertions.assertTrue(result.isPresent(), "Should return a value");
-
-        // Assertions min by length
         Assertions.assertEquals(expectedResult, result.get(),
                 "Should return the name with the shortest length: Ana");
+    }
+
+    @Test
+    @DisplayName("Test Operations: BinaryOperator in reduce by Integer::sum")
+    void testOperationsMapReduceWithBinaryOperatorInteger_ReturnsExpectedValue() {
+        var result = binaryOperatorStream.operationsMapReduceWithBinaryOperatorInteger();
+        var expectedResult = 245;
+
+        // Assertions
+        Assertions.assertInstanceOf(Integer.class, result, "Should return a Integer Instance");
+        Assertions.assertEquals(expectedResult, result, "Should return the correct sum of age");
     }
 }
