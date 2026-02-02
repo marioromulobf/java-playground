d06bbffa76ad083c4806daeb6b674dd1ecb43be3
Mario Fernandes <marioromulof@gmail.com>
2026-02-01 22:15:17 -0300

    feat(stream): UnaryOperatorStream :: iterate

--- java-stream-api-poc/src/main/java/com/mariofernandes/javapoc/stream/UnaryOperatorStream.java
+++ java-stream-api-poc/src/main/java/com/mariofernandes/javapoc/stream/UnaryOperatorStream.java
@@ -1,10 +1,27 @@
 package com.mariofernandes.javapoc.stream;
 
+import java.util.List;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
 public class UnaryOperatorStream {
+
+    public static final List<String> NAMES = List.of("Mario", "Ana", "Pedro", "Mario", "Carla", "Maria");
+    private final UnaryOperator<int[]> fibonacci = v -> new int[]{v[1], v[0] + v[1]};
 
     public UnaryOperatorStream() {}
 
+    public List<Integer> operationsIterateLimitMapToList() {
+        return Stream.iterate(new int[]{0, 1}, fibonacci)
+                .limit(10)
+                .map(v -> v[0])
+                .toList();
+    }
+
     public static void run() {
         UnaryOperatorStream unaryOperatorStream = new UnaryOperatorStream();
+
+        System.out.println("Operations: Iterate - First 10 numbers in Fibonacci sequence -> Result: ");
+        System.out.println(unaryOperatorStream.operationsIterateLimitMapToList());
     }
 }
--- java-stream-api-poc/src/test/java/com/mariofernandes/javapoc/stream/UnaryOperatorStreamTest.java
+++ java-stream-api-poc/src/test/java/com/mariofernandes/javapoc/stream/UnaryOperatorStreamTest.java
@@ -1,6 +1,11 @@
 package com.mariofernandes.javapoc.stream;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class UnaryOperatorStreamTest {
     private UnaryOperatorStream unaryOperatorStream;
@@ -10,5 +15,15 @@ public class UnaryOperatorStreamTest {
         this.unaryOperatorStream = new UnaryOperatorStream();
     }
 
+    @Test
+    @DisplayName("Test Operations: Iterate - First 10 numbers in Fibonacci sequence ")
+    void testOperationsIterateLimitMapToList_ReturnsExpectedValue() {
+        var result = unaryOperatorStream.operationsIterateLimitMapToList();
+        var expectedResult = List.of(0, 1, 1, 2, 3, 5, 8, 13, 21, 34);
 
+        // Assertions
+        Assertions.assertInstanceOf(List.class, result, "Should return a List Instance");
+        Assertions.assertEquals(expectedResult.size(), result.size(), "Should return exactly 10 numbers");
+        Assertions.assertEquals(expectedResult, result, "Should return correct Fibonacci sequence");
+    }
 }
