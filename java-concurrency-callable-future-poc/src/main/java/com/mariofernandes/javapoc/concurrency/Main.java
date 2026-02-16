beac0107c2cbdf31675179a41fd04503a7995b4a
Mario Fernandes <marioromulof@gmail.com>
2026-02-16 09:04:58 -0300

    feat(concurrency): main class created

---
+++ java-concurrency-callable-future-poc/src/main/java/com/mariofernandes/javapoc/concurrency/Main.java
@@ -0,0 +1,9 @@
+package com.mariofernandes.javapoc.concurrency;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("=== Java Concurrency - Callable and Future POC ===\n");
+        CallableFuturePOC.run();
+    }
+}
