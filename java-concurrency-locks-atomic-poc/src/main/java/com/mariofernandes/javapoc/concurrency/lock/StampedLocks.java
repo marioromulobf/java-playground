0d74c223e6f3b2fee790b9dad706d27ec1d15e98
Mario Fernandes <marioromulof@gmail.com>
2026-02-26 00:05:13 -0300

    feat(concurrency): basicStampedLock method and unit test

--- java-concurrency-locks-atomic-poc/src/main/java/com/mariofernandes/javapoc/concurrency/lock/StampedLocks.java
+++ java-concurrency-locks-atomic-poc/src/main/java/com/mariofernandes/javapoc/concurrency/lock/StampedLocks.java
@@ -1,11 +1,66 @@
 package com.mariofernandes.javapoc.concurrency.lock;
 
+import java.math.BigDecimal;
+import java.util.concurrent.locks.StampedLock;
+
 public class StampedLocks {
+
+    private StampedLock stampedLock = new StampedLock();
+    private double x;
+    private double y;
+
     public StampedLocks() {}
+
+    private void move(double deltaX, double deltaY) {
+        long stamp = stampedLock.writeLock();
+        try {
+            x += deltaX;
+            y += deltaY;
+        } finally {
+            stampedLock.unlockWrite(stamp);
+        }
+    }
+
+    private double distanceFromOrigin() {
+        long stamp = stampedLock.tryOptimisticRead();
+        double currentX = x;
+        double currentY = y;
+        if (!stampedLock.validate(stamp)) {
+            stamp = stampedLock.readLock();
+            try {
+                currentX = x;
+                currentY = y;
+            } finally {
+                stampedLock.unlockRead(stamp);
+            }
+        }
+
+        return Math.sqrt(currentX * currentX + currentY * currentY);
+    }
+
+    public double basicStampedLock() throws InterruptedException {
+        var threads = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            threads[i] = Thread.ofVirtual().start(() -> {
+                for (int j = 0; j < 100; j++) {
+                    move(1, 1);
+                    distanceFromOrigin();
+                }
+            });
+        }
+        for (var thread : threads) thread.join();
+
+        return distanceFromOrigin();
+    }
 
     public static void run() {
         StampedLocks stampedLocks = new StampedLocks();
         System.out.println("=== StampedLock ===");
-        
+        try {
+            var finalDistance = stampedLocks.basicStampedLock();
+            System.out.println("Final distance from origin: " + finalDistance);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
     }
 }
--- java-concurrency-locks-atomic-poc/src/test/java/com/mariofernandes/javapoc/concurrency/lock/StampedLocksTest.java
+++ java-concurrency-locks-atomic-poc/src/test/java/com/mariofernandes/javapoc/concurrency/lock/StampedLocksTest.java
@@ -1,6 +1,9 @@
 package com.mariofernandes.javapoc.concurrency.lock;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class StampedLocksTest {
     private StampedLocks stampedLocks;
@@ -10,5 +13,16 @@ public class StampedLocksTest {
         stampedLocks = new StampedLocks();
     }
 
-    
+    @Test
+    @DisplayName("Test operations of StampedLocks: basicStampedLock")
+    void testBasicStampedLock_ShouldReturnExpectedDistance() {
+        var expectedDistance = 1414.213562373095;
+        try {
+            var result = stampedLocks.basicStampedLock();
+
+            Assertions.assertEquals(expectedDistance, result, 0.0001, "Result distance should be approximately equal to expected distance.");
+        } catch (Exception e) {
+            Assertions.fail("An error occurred during the testBasicStampedLock execution: " + e.getMessage());
+        }
+    }
 }
