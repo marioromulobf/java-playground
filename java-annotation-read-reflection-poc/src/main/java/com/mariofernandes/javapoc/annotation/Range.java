bc01e52b63b9c037bfa64ea2255ca35a1c156890
Mario Fernandes <marioromulof@gmail.com>
2026-02-28 15:09:06 -0300

    feat(annotation): Range own annotation created

---
+++ java-annotation-read-reflection-poc/src/main/java/com/mariofernandes/javapoc/annotation/Range.java
@@ -0,0 +1,15 @@
+package com.mariofernandes.javapoc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME) // available at runtime
+@Target(ElementType.FIELD) // only on field
+public @interface Range {
+    int min() default Integer.MIN_VALUE; // min value
+    int max() default Integer.MAX_VALUE; // max value
+
+    String message() default "%s value is out of range"; // string formatting
+}
