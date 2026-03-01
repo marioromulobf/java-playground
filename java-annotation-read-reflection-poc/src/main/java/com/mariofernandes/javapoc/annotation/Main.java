1644629a0cf522b5c072fd8b49c7dac590bd7b94
Mario Fernandes <marioromulof@gmail.com>
2026-03-01 20:32:41 -0300

    feat(annotation): Custom annotations check

--- java-annotation-read-reflection-poc/src/main/java/com/mariofernandes/javapoc/annotation/Main.java
+++ java-annotation-read-reflection-poc/src/main/java/com/mariofernandes/javapoc/annotation/Main.java
@@ -4,5 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("=== Java Annotation - My own Annotations POC ===\n");
+        try {
+            Person person = new Person("Mario", "Brazil", 30, 666);
+            Validator.validate(person);
+            System.out.println("You are a valid person: " + person);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+
+        try {
+            Person person = new Person(null, null,-1, null);
+            Validator.validate(person);
+            System.out.println("You are a invalid person: " + person);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+
+        try {
+            Person person = new Person(null, null,-1, -1);
+            Validator.validate(person);
+            System.out.println("You are a invalid person: " + person);
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
     }
 }
