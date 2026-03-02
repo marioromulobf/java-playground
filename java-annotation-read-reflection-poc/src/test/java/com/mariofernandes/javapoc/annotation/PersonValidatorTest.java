3bb0107bf9749ff5f15d50b727c54f186addb152
Mario Fernandes <marioromulof@gmail.com>
2026-03-01 21:50:25 -0300

    feat(annotation): Unit test ShouldThrowsValidationExceptionWith4Violations created

--- java-annotation-read-reflection-poc/src/test/java/com/mariofernandes/javapoc/annotation/PersonValidatorTest.java
+++ java-annotation-read-reflection-poc/src/test/java/com/mariofernandes/javapoc/annotation/PersonValidatorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class PersonValidatorTest {
     @Test
     @DisplayName("Test Operations of Annotations Validator: ShouldValidateSuccessfully")
@@ -15,5 +17,23 @@ public class PersonValidatorTest {
         } catch (Exception e) {
             Assertions.fail("The testPersonValidator_ShouldValidateSuccessfully was encountered an exception: " + e.getMessage());
         }
+    }
+
+    @Test
+    @DisplayName("Test Operations of Annotations Validator: ShouldThrowsValidationExceptionWith4Violations")
+    void testPersonValidator_ShouldThrowsValidationExceptionWith4Violations() {
+        var expectedMessage = "Validation failed: [name must not be blank, Country is required, age value is out of range (expected: 0..99, got: -1), luckyNumber must not be blank]";
+        var expectedViolations = List.of(
+                "name must not be blank",
+                "Country is required",
+                "age value is out of range (expected: 0..99, got: -1)",
+                "luckyNumber must not be blank"
+        );
+        Person person = new Person(null, null, -1, null);
+        ValidationException exception = Assertions.assertThrowsExactly(ValidationException.class, () -> Validator.validate(person));
+
+        Assertions.assertEquals(expectedMessage, exception.getMessage(), "Exception message should be equal to expected value");
+        Assertions.assertEquals(expectedViolations.size(), exception.getViolations().size(), "Number of violations should be 4");
+        Assertions.assertEquals(expectedViolations, exception.getViolations(), "Violations should be equal to expected values");
     }
 }
