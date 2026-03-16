1b36d2faa2a4345b3346a021bb4f2ab32b9b6c20
Mario Fernandes <marioromulof@gmail.com>
2026-03-15 23:50:52 -0300

    feat(skiplists): main class

--- java-skip-lists-poc/src/main/java/com/mariofernandes/javapoc/skiplists/Main.java
+++ java-skip-lists-poc/src/main/java/com/mariofernandes/javapoc/skiplists/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("=== Java Skip Lists POC ===\n");
+        SkipLists skipLists = new SkipLists(16);
+        skipLists.insert(3, 3L);
+        skipLists.insert(6, 6L);
+        skipLists.insert(7, 7L);
+        skipLists.insert(9, 9L);
+        skipLists.insert(12, 12L);
+        skipLists.insert(19, 19L);
+        skipLists.insert(17, 17L);
+        skipLists.insert(26, 26L);
+        skipLists.insert(21, 21L);
+        skipLists.insert(25, 25L);
+        System.out.println(skipLists);
+        System.out.println("Search 19 result: " + skipLists.search(19));
+        System.out.println("Search 30 result: " + skipLists.search(30));
+        System.out.println("Delete 21");
+        skipLists.delete(21);
+        System.out.println(skipLists);
     }
 }
