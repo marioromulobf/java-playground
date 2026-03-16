1ef703d88e7651c637c823585668268dccfccf27
Mario Fernandes <marioromulof@gmail.com>
2026-03-15 23:42:47 -0300

    feat(skiplists): node class

---
+++ java-skip-lists-poc/src/main/java/com/mariofernandes/javapoc/skiplists/Node.java
@@ -0,0 +1,36 @@
+package com.mariofernandes.javapoc.skiplists;
+
+public class Node {
+    private int key;
+    private Long value;
+    Node[] forward;
+
+    public Node(int key, Long value, int level) {
+        this.key = key;
+        this.value = value;
+        this.forward = new Node[level + 1];
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    public void setValue(Long value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{key=")
+                .append(key)
+                .append(", value=")
+                .append(value)
+                .append("}");
+        return sb.toString();
+    }
+}
