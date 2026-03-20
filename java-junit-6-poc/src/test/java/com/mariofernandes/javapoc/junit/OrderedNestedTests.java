15ba2a3ac4b65317c19799af683fe2ad77fc4a03
Mario Fernandes <marioromulof@gmail.com>
2026-03-20 08:25:39 -0300

    feat(junit6): Ordered Nested testes created

---
+++ java-junit-6-poc/src/test/java/com/mariofernandes/javapoc/junit/OrderedNestedTests.java
@@ -0,0 +1,29 @@
+package com.mariofernandes.javapoc.junit;
+
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
+
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+public class OrderedNestedTests {
+
+    @Nested
+    @Order(1)
+    class PrimaryTests {
+        @Test
+        void test1() {
+
+        }
+    }
+
+    @Nested
+    @Order(2)
+    class SecondaryTests {
+        @Test
+        void test2() {
+
+        }
+    }
+}
