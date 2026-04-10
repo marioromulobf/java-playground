13bf4d41ec9f99139fa3d733f247cfb6eadb1717
Mario Fernandes <marioromulof@gmail.com>
2026-04-09 22:11:44 -0300

    feat(sep): dpk03 implementation 7 created

---
+++ java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk03/DPK03Impl7.java
@@ -0,0 +1,57 @@
+package com.mariofernandes.javapoc.sep.dpk03;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DPK03Impl7 {
+
+    private final Map<Integer, String> mapIntegerKey = new HashMap<>();
+    private final Map<String, String> mapStringKey = new HashMap<>();
+
+    public void add(int key, String value) {
+        mapIntegerKey.put(key, value);
+    }
+
+    public void add(String key, String value) {
+        mapStringKey.put(key, value);
+    }
+
+    public String lookup(int key) {
+        return mapIntegerKey.get(key);
+    }
+
+    public String lookup(String key) {
+        var result = mapStringKey.get(key);
+
+        if (result == null) {
+            result = mapStringKey.entrySet().stream()
+                    .filter(entry -> entry.getValue().equals(key))
+                    .map(Map.Entry::getKey)
+                    .findFirst()
+                    .orElse(null);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--> DPK 03 - Implementation 04 <--");
+        DPK03Impl7 dpk03Impl7 = new DPK03Impl7();
+        dpk03Impl7.add(1, "John");
+        dpk03Impl7.add("John", "john@john.john.com");
+        dpk03Impl7.add(2, "Mario");
+        dpk03Impl7.add("Mario", "mario@mail.com");
+        dpk03Impl7.add(3, "Romulo");
+        dpk03Impl7.add("Romulo", "romulo@mail.com");
+
+        var result = dpk03Impl7.lookup(1);
+        System.out.println("lookup(1) -> " + result);
+
+        var resultByName = dpk03Impl7.lookup("John");
+        System.out.println("lookup(\"John\") -> " + resultByName);
+
+        var resultByMail = dpk03Impl7.lookup("john@john.john.com");
+        System.out.println("lookup(\"john@john.john.com\") -> " + resultByMail);
+    }
+}
+
