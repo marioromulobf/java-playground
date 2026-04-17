a71c6d275fadbbbf872a393185addcabba4eda26
Mario Fernandes <marioromulof@gmail.com>
2026-04-17 10:09:03 -0300

    feat(sep): dpk04 implementation 10 created

---
+++ java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk04/DPK04Impl10.java
@@ -0,0 +1,33 @@
+package com.mariofernandes.javapoc.sep.dpk04;
+
+import java.util.List;
+
+public class DPK04Impl10 {
+    List<Country> countries = List.of(
+            new Country("Usa", "English"),
+            new Country("Brazil", "Portuguese"),
+            new Country("Spain", "Spanish"),
+            new Country("Italy", "Italian"),
+            new Country("France", "French"),
+            new Country("Germany", "German")
+    );
+
+    public String patternMatcher(String country) {
+        return countries.stream()
+                .filter(item -> item.country().equals(country))
+                .map(Country::language)
+                .findFirst()
+                .orElse("Unknown");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--> DPK 04 - Implementation 10 <--");
+        DPK04Impl10 dkp04Impl10 = new DPK04Impl10();
+
+        var result = dkp04Impl10.patternMatcher("Usa");
+        System.out.println("pattern_matcher(\"Usa\") -> " + result);
+    }
+
+    public record Country(String country, String language) {}
+}
+
