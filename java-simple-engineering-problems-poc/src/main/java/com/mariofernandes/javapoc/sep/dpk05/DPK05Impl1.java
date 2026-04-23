2e7ffda564ba0d296d10554eb30aaab4b6b68d06
Mario Fernandes <marioromulof@gmail.com>
2026-04-22 22:34:57 -0300

    feat(sep): DPK05 implementation 1 created

--- java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk05/DPK05Impl1.java
+++ java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk05/DPK05Impl1.java
@@ -5,11 +5,25 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class DPK05Impl1 {
+
+    private int matchesPlayed;
+
+    public DPK05Impl1() {
+        matchesPlayed = 0;
+    }
+
+    public static final String[] PLAYERS = {"John", "Paul", "George", "Ringo"};
     Map<String, Integer> power = Map.of(
             "John", 100,
             "Paul", 90,
             "George", 80,
             "Ringo", 70
+    );
+
+    Map<Integer, int[]> points = Map.of(
+            -1, new int[]{-5, 10},
+            0, new int[]{5, 5},
+            1, new int[]{10, -5}
     );
 
     Map<String, Integer> leaderboard = new HashMap<>(Map.of(
@@ -23,27 +37,22 @@ public class DPK05Impl1 {
         return power.get(name);
     }
 
-    public String getMostPowerful(String playerOne, String playerTwo) {
-        var result = lookupPower(playerOne).compareTo(lookupPower(playerTwo));
-        return result == 0 ? "-" : (result > 0 ? playerOne : playerTwo);
+    public int getMostPowerful(String playerOne, String playerTwo) {
+        return lookupPower(playerOne).compareTo(lookupPower(playerTwo));
     }
 
     public String play(String playerOne, String playerTwo) {
-        var winner = getMostPowerful(playerOne, playerTwo);
-        var pointsP1 = leaderboard.get(playerOne);
-        var pointsP2 = leaderboard.get(playerTwo);
+        var resultMostPowerful = getMostPowerful(playerOne, playerTwo);
+        var finalPoints = points.get(resultMostPowerful);
+        var pointsP1 = leaderboard.get(playerOne) + finalPoints[0];
+        var pointsP2 = leaderboard.get(playerTwo) + finalPoints[1];
+        var options = Map.of(
+                -1, playerTwo,
+                0, "It was a draw!",
+                1, playerOne
+        );
 
-        if (winner.equals("-")) {
-            winner = "It was a draw!";
-            pointsP1 += 5;
-            pointsP2 += 5;
-        } else if (winner.equals(playerOne)) {
-            pointsP1 += 10;
-            pointsP2 -= 5;
-        } else {
-            pointsP1 -= 5;
-            pointsP2 += 10;
-        }
+        var winner = options.get(resultMostPowerful);
         System.out.println("play(\"" + playerOne + "\", \"" + playerTwo + "\") -> \"" + winner + "\"");
 
         leaderboard.put(playerOne, pointsP1);
@@ -55,6 +64,13 @@ public class DPK05Impl1 {
 
         System.out.println(log);
 
+        matchesPlayed++;
+
+        if (matchesPlayed > 2) {
+            matchesPlayed = 0;
+            leaderboard.replaceAll((k, v) -> 0);
+        }
+
         return winner;
     }
 
@@ -62,8 +78,9 @@ public class DPK05Impl1 {
         System.out.println("--> DPK 05 - Implementation 01 <--");
         DPK05Impl1 dkp05Impl1 = new DPK05Impl1();
 
-        var result = dkp05Impl1.play("John", "Paul");
-        dkp05Impl1.play("John", "Ringo");
+        var winner1 = dkp05Impl1.play(DPK05Impl1.PLAYERS[0], DPK05Impl1.PLAYERS[1]);
+        var winner2 = dkp05Impl1.play(DPK05Impl1.PLAYERS[2], DPK05Impl1.PLAYERS[3]);
+        var finalWinner = dkp05Impl1.play(winner1, winner2);
     }
 }
 
