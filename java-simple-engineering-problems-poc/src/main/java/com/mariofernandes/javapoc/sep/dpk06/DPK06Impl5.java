c9a1309299d60dbbdf2cc380859c68b1e0dbf58c
Mario Fernandes <marioromulof@gmail.com>
2026-04-29 23:58:01 -0300

    feat(sep): DPK06 implementation 5 created

---
+++ java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk06/DPK06Impl5.java
@@ -0,0 +1,67 @@
+package com.mariofernandes.javapoc.sep.dpk06;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DPK06Impl5 {
+
+    public List<String> tokenize(String data, String token) {
+        List<String> result = new ArrayList<>();
+
+        if (data == null || data.isEmpty() || token == null || token.isEmpty()) {
+            return result;
+        }
+
+        int start = 0;
+
+        for (int i = 0; i < data.length();) {
+            if (matchToken(data, token, i)) {
+                result.add(extract(data, start, i));
+                i += token.length();
+                start = i;
+            } else {
+                i++;
+            }
+        }
+        result.add(extract(data, start, data.length()));
+
+        return result;
+    }
+
+    private boolean matchToken(String data, String token, int index) {
+        for (int i = 0; i < token.length(); i++) {
+            if (data.charAt(index + i) != token.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String extract(String data, int start, int end) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = start; i < end; i++) {
+            sb.append(data.charAt(i));
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--> DPK 06 - Implementation 05 <--");
+        DPK06Impl5 dpk06Impl5 = new DPK06Impl5();
+
+        var result = dpk06Impl5.tokenize("Hello,World,How,Are,You", ",");
+        System.out.println("tokenize(\"Hello,World,How,Are,You\", \",\") -> " + result);
+
+        result = dpk06Impl5.tokenize("Hello World How Are You", " ");
+        System.out.println("tokenize(\"Hello World How Are You\", \" \") -> " + result);
+
+        result = dpk06Impl5.tokenize("Hello-World-How-Are-You", "-");
+        System.out.println("tokenize(\"Hello-World-How-Are-You\", \"-\") -> " + result);
+
+        result = dpk06Impl5.tokenize("Hello--World--How--Are--You", "--");
+        System.out.println("tokenize(\"Hello--World--How--Are--You\", \"--\") -> " + result);
+    }
+}
+
