1e432a0b582968f05a1d628ad34e0ce9e7a92a97
Mario Fernandes <marioromulof@gmail.com>
2026-05-02 23:26:56 -0300

    feat(sep): DPK07 implementation 1 created

---
+++ java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk07/DPK07Impl1.java
@@ -0,0 +1,46 @@
+package com.mariofernandes.javapoc.sep.dpk07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DPK07Impl1 {
+
+    public<T> List<List<T>> groupBy(List<T> number, int sizeGroup) {
+        List<List<T>> result = new ArrayList<>();
+
+        if (number.size() <= sizeGroup) {
+            result.add(number);
+            return result;
+        }
+
+        List<T> subList = new ArrayList<>(sizeGroup);
+        int countBySizeGroup = 0;
+        for (int i = 0; (countBySizeGroup + i) < number.size(); i++) {
+            if (subList.size() == sizeGroup) {
+                result.add(subList);
+                subList = new ArrayList<>(sizeGroup);
+                i = 0;
+                countBySizeGroup = result.size() * sizeGroup;
+            }
+            subList.add(number.get(countBySizeGroup + i));
+        }
+
+        if (subList.size() > 0) {
+            result.add(subList);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--> DPK 07 - Implementation 01 <--");
+        DPK07Impl1 dpk07Impl1 = new DPK07Impl1();
+
+        var result = dpk07Impl1.groupBy(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 3);
+        System.out.println("groupBy([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3) -> " + result);
+
+        var result2 = dpk07Impl1.groupBy(List.of("a", "b", "c", "d", "e", "f", "g", "h", "i", "j"), 3);
+        System.out.println("groupBy([\"a\",\"b\",\"c\",\"d\",\"e\",\"f\",\"g\",\"h\",\"i\",\"j\"], 3) -> " + result2);
+    }
+}
+
---
+++ java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk07/TABLE_TEST.md
@@ -0,0 +1,17 @@
+# Problem
+This is a table test for *DPK07 - Group Bu*:
+
+**Create a function that can group a list of numbers by a given number.**
+```
+group_by([1,2,3,4,5,6,7,8,9,10], 3) -> [[1,2,3], [4,5,6], [7,8,9], [10]]
+```
+
+**Please make sure the same function works with strings as well.**
+```
+group_by(["a","b","c","d","e","f","g","h","i","j"], 3) -> [["a","b","c"], ["d","e","f"], ["g","h","i"], ["j"]]
+```
+
+**Can you refactor the code and create your own group by function, do not use any prebuild function.**
+
+## Table Test - Implementation 1
+
