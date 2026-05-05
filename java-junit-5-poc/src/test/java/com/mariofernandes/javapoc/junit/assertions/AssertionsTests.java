9a66e24ef223d6032bcab334aa16ad9746b1c0c5
Mario Fernandes <marioromulof@gmail.com>
2026-05-04 21:47:34 -0300

    feat(junit5): assertions tests

---
+++ java-junit-5-poc/src/test/java/com/mariofernandes/javapoc/junit/assertions/AssertionsTests.java
@@ -0,0 +1,129 @@
+package com.mariofernandes.javapoc.junit.assertions;
+
+import com.mariofernandes.javapoc.junit.Calculator;
+import com.mariofernandes.javapoc.junit.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class AssertionsTests {
+
+    private final Calculator calculator = new Calculator();
+    private final Person person = new Person("Jane", "Doe");
+
+    @Test
+    void standardAssertions() {
+        Assertions.assertEquals(2, calculator.add(1, 1));
+        Assertions.assertEquals(4, calculator.multiply(2, 2),
+                "The optional failure message is now the last parameter");
+
+        // Lazily evaluates generateFailureMessage('a', 'b').
+        Assertions.assertTrue('a' < 'b', () -> generateFailureMessage('a', 'b'));
+    }
+
+    @Test
+    void groupedAssertions() {
+        // In a grouped assertion all assertions are executed,
+        // and all failures will be reported together.
+        Assertions.assertAll("person",
+                () -> Assertions.assertEquals("Jane", person.getFirstName()),
+                () -> Assertions.assertEquals("Doe", person.getLastName())
+        );
+    }
+
+    @Test
+    void dependentAssertions() {
+        // Within a code block, if an assertion fails the
+        // subsequent code in the same block will be skipped.
+        Assertions.assertAll("properties",
+                () -> {
+                    String firstName = person.getFirstName();
+                    Assertions.assertNotNull(firstName);
+
+                    // Executed only if the previous assertion is valid.
+                    Assertions.assertAll("first name",
+                            () -> Assertions.assertTrue(firstName.startsWith("J")),
+                            () -> Assertions.assertTrue(firstName.endsWith("e"))
+                    );
+                },
+                () -> {
+                    // Grouped assertion, so processed independently
+                    // of results of first name assertions.
+                    String lastName = person.getLastName();
+                    Assertions.assertNotNull(lastName);
+
+                    // Executed only if the previous assertion is valid.
+                    Assertions.assertAll("last name",
+                            () -> Assertions.assertTrue(lastName.startsWith("D")),
+                            () -> Assertions.assertTrue(lastName.endsWith("e"))
+                    );
+                }
+        );
+    }
+
+    @Test
+    void exceptionTesting() {
+        Exception exception = Assertions.assertThrows(ArithmeticException.class,
+                () -> calculator.divide(1, 0));
+        Assertions.assertEquals("/ by zero", exception.getMessage());
+    }
+
+    @Test
+    void timeoutNotExceeded() {
+        // The following assertion succeeds.
+        Assertions.assertTimeout(Duration.ofMinutes(2), () -> {
+            // Perform task that takes less than 2 minutes.
+        });
+    }
+
+    @Test
+    void timeoutNotExceededWithResult() {
+        // The following assertion succeeds, and returns the supplied object.
+        String actualResult = Assertions.assertTimeout(Duration.ofMinutes(2), () -> {
+            TimeUnit.SECONDS.sleep(2);
+            return "a result";
+        });
+        Assertions.assertEquals("a result", actualResult);
+    }
+
+    @Test
+    void timeoutNotExceededWithMethod() {
+        // The following assertion invokes a method reference and returns an object.
+        String actualGreeting = Assertions.assertTimeout(Duration.ofMinutes(2),
+                AssertionsTests::greeting);
+        Assertions.assertEquals("Hello, World!", actualGreeting);
+    }
+
+    @Test
+    void timeoutExceeded() {
+        // The following assertion fails with an error message similar to:
+        // execution exceeded timeout of 10 ms by 90 ms.
+        Assertions.assertTimeout(Duration.ofMillis(10), () -> {
+            // Simulate task that takes more than 10 ms.
+            Thread.sleep(100);
+        });
+    }
+
+    @Test
+    void timeoutExceededWithPreemptiveTermination() {
+        // The following assertion fails with an error message similar to:
+        // execution timed out after 10 ms.
+        Assertions.assertTimeoutPreemptively(Duration.ofMillis(10), () -> {
+            // Simulate task that takes more than 10 ms.
+            new CountDownLatch(1).await();
+        });
+    }
+
+    private static String greeting() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
+        return "Hello, World!";
+    }
+
+    private String generateFailureMessage(char a, char b) {
+        return "Assertion messages can be lazily evaluated -- "
+                + "to avoid constructing complex messages unnecessarily. " + (a < b);
+    }
+}
