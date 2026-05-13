06eb2511864db8589e8a5b7e9c342808916480d3
Mario Fernandes <marioromulof@gmail.com>
2026-05-12 21:41:26 -0300

    feat(sep): DPK08 implementation 2 created

--- java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk08/DPK08Impl2.java
+++ java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk08/DPK08Impl2.java
@@ -27,13 +27,13 @@ public class DPK08Impl2 {
     }
 
     public static void main(String[] args) {
-        System.out.println("--> DPK 08 - Implementation 01 <--");
-        DPK08Impl2 dpk08Impl1 = new DPK08Impl2();
+        System.out.println("--> DPK 08 - Implementation 02 <--");
+        DPK08Impl2 dpk08Impl2 = new DPK08Impl2();
 
-        var result = dpk08Impl1.map(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), (x) -> x * 2);
+        var result = dpk08Impl2.map(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), (x) -> x * 2);
         System.out.println("map([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], (x) -> x * 2) -> " + result);
 
-        var result2 = dpk08Impl1.map(new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}, (term) -> "term: " + term);
+        var result2 = dpk08Impl2.map(new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}, (term) -> "term: " + term);
         System.out.println("map([\"a\",\"b\",\"c\",\"d\",\"e\",\"f\",\"g\",\"h\",\"i\",\"j\"], (term) -> \"term: \" + term) -> " + result2);
     }
 }
