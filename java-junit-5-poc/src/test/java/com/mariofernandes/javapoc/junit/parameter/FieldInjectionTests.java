8e1be8c53af7b1386b03d3e5dc8a5a98a40bcdc9
Mario Fernandes <marioromulof@gmail.com>
2026-05-26 20:45:44 -0300

    feat(junit5): parameterized class tests create

---
+++ java-junit-5-poc/src/test/java/com/mariofernandes/javapoc/junit/parameter/FieldInjectionTests.java
@@ -0,0 +1,28 @@
+package com.mariofernandes.javapoc.junit.parameter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.Parameter;
+import org.junit.jupiter.params.ParameterizedClass;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+@ParameterizedClass
+@CsvSource({"apple, 23", "banana, 42"})
+public class FieldInjectionTests {
+    @Parameter(0)
+    String fruit;
+
+    @Parameter(1)
+    int quantity;
+
+    @Test
+    void test() {
+        Assertions.assertTrue(List.of("apple", "banana").contains(fruit));
+        Assertions.assertTrue(List.of(23, 42).contains(quantity));
+    }
+
+    @Test
+    void testAnother() {}
+}
