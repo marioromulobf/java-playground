02c564541c0ddb98844c1933212d11521a77177c
Mario Fernandes <marioromulof@gmail.com>
2026-05-28 22:33:45 -0300

    feat(sep): DPK11 implementation 6 created

--- java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk11/DPK11Impl6.java
+++ java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk11/DPK11Impl6.java
@@ -2,17 +2,17 @@ package com.mariofernandes.javapoc.sep.dpk11;
 
 public class DPK11Impl6 {
 
-    private final TokenMacher tokenMacher;
+    private final TokenMatcher tokenMatcher;
 
-    public DPK11Impl6(TokenMacher tokenMacher) {
-        this.tokenMacher = tokenMacher;
+    public DPK11Impl6(TokenMatcher tokenMatcher) {
+        this.tokenMatcher = tokenMatcher;
     }
 
-    public interface TokenMacher {
+    public interface TokenMatcher {
         boolean matchesAt(String source, String token, int start);
     }
 
-    public static class ExtractTokenMacher implements TokenMacher {
+    public static class ExtractTokenMatcher implements TokenMatcher {
         @Override
         public boolean matchesAt(String source, String token, int start) {
             if (start + token.length() > source.length()) {
@@ -33,7 +33,7 @@ public class DPK11Impl6 {
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < source.length();) {
-            if (tokenMacher.matchesAt(source, oldToken, i)) {
+            if (tokenMatcher.matchesAt(source, oldToken, i)) {
                 result.append(newToken);
                 i += oldToken.length();
             } else {
@@ -57,7 +57,7 @@ public class DPK11Impl6 {
 
     public static void main(String[] args) {
         System.out.println("--> DPK 11 - Implementation 06 <--");
-        DPK11Impl6 dpk11Impl6 = new DPK11Impl6(new ExtractTokenMacher());
+        DPK11Impl6 dpk11Impl6 = new DPK11Impl6(new ExtractTokenMatcher());
 
         var input1 = "Hello,World,How,Are,You";
         var result1 = dpk11Impl6.replace(input1, ",", "-");
