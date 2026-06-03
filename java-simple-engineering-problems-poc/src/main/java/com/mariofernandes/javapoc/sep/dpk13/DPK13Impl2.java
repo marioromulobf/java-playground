04921718c539dc92bad66b490c0455f44a8dd4f1
Mario Fernandes <marioromulof@gmail.com>
2026-06-02 22:55:02 -0300

    feat(sep): DPK13 implementation 2 created

---
+++ java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk13/DPK13Impl2.java
@@ -0,0 +1,50 @@
+package com.mariofernandes.javapoc.sep.dpk13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DPK13Impl2 {
+
+    public List<Object> fizzbuzz() {
+        return fizzbuzz(100);
+    }
+
+    public List<Object> fizzbuzz(int size) {
+        var result = new ArrayList<>();
+
+        for (int i = 1; i <= size; i++) {
+            result.add(convert(i));
+        }
+
+        return result;
+    }
+
+    private Object convert(int value) {
+        boolean isFizz = value % 3 == 0;
+        boolean isBuzz = value  % 5 == 0;
+
+        if (isFizz && isBuzz) {
+            return "FizzBuzz";
+        }
+        if (isFizz) {
+            return "Fizz";
+        }
+        if (isBuzz) {
+            return "Buzz";
+        }
+
+        return value;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--> DPK 13 - Implementation 01 <--");
+        DPK13Impl2 dpk13Impl1 = new DPK13Impl2();
+
+        var result = dpk13Impl1.fizzbuzz();
+        System.out.println("fizzbuzz() -> " + result);
+
+        var result2 = dpk13Impl1.fizzbuzz(10);
+        System.out.println("fizzbuzz(10) -> " + result2);
+    }
+}
+
