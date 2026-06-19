c7797bc082f1b579cdcbddd4dde9be1e660807ee
Mario Fernandes <marioromulof@gmail.com>
2026-06-19 09:46:58 -0300

    feat(sep): DPK15 implementation 1 created

--- java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk15/DPK15Impl1.java
+++ java-simple-engineering-problems-poc/src/main/java/com/mariofernandes/javapoc/sep/dpk15/DPK15Impl1.java
@@ -4,6 +4,103 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DPK15Impl1 {
+
+    static class Person {
+        private final String name;
+        private final int age;
+
+        private final Friendships friendships;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+            this.friendships = new Friendships();
+        }
+
+        public Friendships getFriendships() {
+            return friendships;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+
+    static class Friendships {
+
+        private List<Person> friends;
+
+        public Friendships() {
+            this.friends = new ArrayList<>();
+        }
+
+        public void addFriend(Person friend) {
+            if (!friends.contains(friend)) {
+                friends.add(friend);
+            }
+        }
+
+        public boolean removeFriend(Person friend) {
+            return friends.remove(friend);
+        }
+
+        public List<Person> getFriends() {
+            return friends;
+        }
+    }
+
+    static class SocialNetwork {
+        public Person getPersonWithMoreFriends(List<Person> people) {
+            Person personWithMoreFriends = null;
+            int maxFriends = -1;
+
+            for (Person person : people) {
+                int friendCount = person.getFriendships().getFriends().size();
+                if (friendCount > maxFriends) {
+                    maxFriends = friendCount;
+                    personWithMoreFriends = person;
+                }
+            }
+
+            return personWithMoreFriends;
+        }
+
+        public Person getPersonWithLessFriends(List<Person> people) {
+            Person personWithLessFriends = null;
+            int minFriends = Integer.MAX_VALUE;
+
+            for (Person person : people) {
+                int friendCount = person.getFriendships().getFriends().size();
+                if (friendCount < minFriends) {
+                    minFriends = friendCount;
+                    personWithLessFriends = person;
+                }
+            }
+
+            return personWithLessFriends;
+        }
+
+        public Person getPersonWithOldestFriend(List<Person> people) {
+            Person personWithOldestFriend = null;
+            int maxAge = -1;
+
+            for (Person person : people) {
+                for (Person friend : person.getFriendships().getFriends()) {
+                    if (friend.getAge() > maxAge) {
+                        maxAge = friend.getAge();
+                        personWithOldestFriend = person;
+                    }
+                }
+            }
+
+            return personWithOldestFriend;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("--> DPK 15 - Implementation 01 <--");
         var mario = new Person("Mario", 22);
@@ -18,13 +115,13 @@ public class DPK15Impl1 {
         var creusa = new Person("Creusa", 31);
         var luffy = new Person("Luffy", 32);
         var nami = new Person("Nami", 33);
-        var zorro = new Person("Zorro", 34);
-        var sanjia = new Person("Sanjia", 35);
-        var usop = new Person("Usop", 36);
-        var frankly = new Person("Frankly", 37);
+        var zoro = new Person("Zoro", 34);
+        var sanji = new Person("Sanji", 35);
+        var usopp = new Person("Usopp", 36);
+        var franky = new Person("Franky", 37);
         var brook = new Person("Brook", 38);
 
-        var people = List.of(mario, romulo, maria, jurubeba, monica, cebolinha, cascao, magalina, carlos, creusa, luffy, nami, zorro, sanjia, usop, frankly, brook);
+        var people = List.of(mario, romulo, maria, jurubeba, monica, cebolinha, cascao, magalina, carlos, creusa, luffy, nami, zoro, sanji, usopp, franky, brook);
 
         mario.getFriendships().addFriend(romulo);
         mario.getFriendships().addFriend(maria);
@@ -45,13 +142,13 @@ public class DPK15Impl1 {
         carlos.getFriendships().addFriend(creusa);
         creusa.getFriendships().addFriend(carlos);
         luffy.getFriendships().addFriend(nami);
-        luffy.getFriendships().addFriend(zorro);
-        luffy.getFriendships().addFriend(sanjia);
+        luffy.getFriendships().addFriend(zoro);
+        luffy.getFriendships().addFriend(sanji);
         nami.getFriendships().addFriend(luffy);
-        zorro.getFriendships().addFriend(luffy);
-        sanjia.getFriendships().addFriend(luffy);
-        usop.getFriendships().addFriend(frankly);
-        frankly.getFriendships().addFriend(usop);
+        zoro.getFriendships().addFriend(luffy);
+        sanji.getFriendships().addFriend(luffy);
+        usopp.getFriendships().addFriend(franky);
+        franky.getFriendships().addFriend(usopp);
         brook.getFriendships().addFriend(mario);
 
         var socialNetwork = new SocialNetwork();
@@ -64,101 +161,3 @@ public class DPK15Impl1 {
         System.out.println("Person with oldest friend: " + personWithOldestFriend.getName());
     }
 }
-
-class Person {
-    private final String name;
-    private final int age;
-
-    private final Friendships friendships;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-        this.friendships = new Friendships();
-    }
-
-    public Friendships getFriendships() {
-        return friendships;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-}
-
-class Friendships {
-
-    private List<Person> friends;
-
-    public Friendships() {
-        this.friends = new ArrayList<>();
-    }
-
-    public void addFriend(Person friend) {
-        if (!friends.contains(friend)) {
-            friends.add(friend);
-        }
-    }
-
-    public boolean removeFriend(Person friend) {
-        return friends.remove(friend);
-    }
-
-    public List<Person> getFriends() {
-        return friends;
-    }
-}
-
-class SocialNetwork {
-    public Person getPersonWithMoreFriends(List<Person> people) {
-        Person personWithMoreFriends = null;
-        int maxFriends = -1;
-
-        for (Person person : people) {
-            int friendCount = person.getFriendships().getFriends().size();
-            if (friendCount > maxFriends) {
-                maxFriends = friendCount;
-                personWithMoreFriends = person;
-            }
-        }
-
-        return personWithMoreFriends;
-    }
-
-    public Person getPersonWithLessFriends(List<Person> people) {
-        Person personWithLessFriends = null;
-        int minFriends = Integer.MAX_VALUE;
-
-        for (Person person : people) {
-            int friendCount = person.getFriendships().getFriends().size();
-            if (friendCount < minFriends) {
-                minFriends = friendCount;
-                personWithLessFriends = person;
-            }
-        }
-
-        return personWithLessFriends;
-    }
-
-    public Person getPersonWithOldestFriend(List<Person> people) {
-        Person personWithOldestFriend = null;
-        int maxAge = -1;
-
-        for (Person person : people) {
-            for (Person friend : person.getFriendships().getFriends()) {
-                if (friend.getAge() > maxAge) {
-                    maxAge = friend.getAge();
-                    personWithOldestFriend = person;
-                }
-            }
-        }
-
-        return personWithOldestFriend;
-    }
-}
-
-
