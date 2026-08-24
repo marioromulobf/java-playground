7e304488135e1a2bfc6eb7e44e62e96e83d8fbd0
Mario Fernandes <marioromulof@gmail.com>
2026-08-24 09:02:47 -0300

    java(quartz): create poc3 3 examples

--- java-quartz-poc/src/main/java/com/mariofernandes/javapoc/quartz/poc/POC03Configuration.java
+++ java-quartz-poc/src/main/java/com/mariofernandes/javapoc/quartz/poc/POC03Configuration.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.TimeZone;
+
 @Configuration
 public class POC03Configuration {
     @Bean
@@ -30,6 +32,30 @@ public class POC03Configuration {
                 .forJob(cronJobDetail)
                 .withIdentity("trigger.A.every10SecondsNoEnd", "poc03")
                 .withSchedule(CronScheduleBuilder.cronSchedule("0/10 * * * * ?"))
+                .build();
+    }
+
+    @Bean
+    public Trigger triggerPoc03B(@Qualifier("cronJobDetail") JobDetail cronJobDetail) {
+        // Define a Trigger for the CronJob, which will execute the job every weekday at 9 AM.
+        // Scheduled: seconds, minutes, hours, day of month, month, day of week, year (optional)
+        return TriggerBuilder.newTrigger()
+                .forJob(cronJobDetail)
+                .withIdentity("trigger.B.everyWeekdayAt9AM", "poc03")
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 0 9 ? * MON-FRI"))
+                .build();
+    }
+
+    @Bean
+    public Trigger triggerPoc03C(@Qualifier("cronJobDetail") JobDetail cronJobDetail) {
+        // Define a Trigger for the CronJob, which will execute the job every
+        // 15 minutes between 8 AM and 5 PM on weekdays, in the "America/New_York" time zone.
+        // Scheduled: seconds, minutes, hours, day of month, month, day of week, year (optional)
+        return TriggerBuilder.newTrigger()
+                .forJob(cronJobDetail)
+                .withIdentity("trigger.C.every15MinutesBetween8And5OnWeekdays", "poc03")
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 0/15 8-17 ? * MON-FRI")
+                        .inTimeZone(TimeZone.getTimeZone("America/New_York")))
                 .build();
     }
 }
